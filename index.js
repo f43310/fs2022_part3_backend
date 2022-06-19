fc5eb860c849106e252d8209225314c4ef23567e
linda8850 <lijia9988@qq.com>
2022-06-19 21:30:36 +0800

    part3->exercises3.2 have done!

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(
+    `<h2>Phonebook has info for ${
+      persons.length
+    } people</h2><h2>${new Date()}</h2>`
+  )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
