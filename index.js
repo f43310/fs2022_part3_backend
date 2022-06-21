7acd1347cc44e6eee82ea2f378fd420d8829371b
linda8850 <lijia9988@qq.com>
2022-06-21 18:25:43 +0800

    part3->exercises3.10 have done!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 morgan.token('data', function (req, res) {
@@ -9,6 +10,8 @@ morgan.token('data', function (req, res) {
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 )
+
+app.use(cors())
 
 let persons = [
   {
@@ -111,7 +114,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
