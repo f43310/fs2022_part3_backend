7ba23aea2b6a84e7f5f18b2754e7b7ed7b1d8a09
linda8850 <lijia9988@qq.com>
2022-06-23 23:19:06 +0800

    提交ignore 增加 .env.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/phonebook')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -16,35 +19,14 @@ app.use(
 
 app.use(cors())
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((people) => {
+    response.json(people)
+  })
 })
 
 app.get('/info', (request, response) => {
